7be005a6e80b6824adb88a4f046d44d52e1e5851
ALISHER KARIBZHANOV <alisherkaribzhanov@ALISHERs-MacBook-Pro-2.local>
2023-05-01 19:45:45 +0600

    Two queens problem

---
+++ content/03. Efficiency/02. Two Queens Problem/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+#define forn(i, a, b) for(int i = a; i < b; i++)
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int N;
+    cin >> N;
+    unordered_map<int, int> q;
+
+    q[1] = q[2] = 0;
+    forn(i, 3, N+1) {
+        q[i] = q[i-1] + (2*i-1) * (i*i - 3*(i-1) - 1) - (i-1)*(i-2);
+    }
+
+    cout << q[N];
+}
